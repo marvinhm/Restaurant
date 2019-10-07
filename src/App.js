a33f80afa3f1a157e2bee7fd711f0455e4750619
Marvin Matovu <marvinmatovu@gmail.com>
2019-10-07 03:05:30 +0100

    setup for react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+    render() {
+        return (
+            <div className="hello">
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default App
