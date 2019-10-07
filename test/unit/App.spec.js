2ea6a62a1cdf08fed9fd03da9b51e3acd12a8fdd
Marvin Matovu <marvinmatovu@gmail.com>
2019-10-07 14:41:44 +0100

    Set up enzyme

---
+++ test/unit/App.spec.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { mount } from 'enzyme';
+import App from '../../src/App';
+
+describe("App", () => {
+    it("Renders Hello", () => {
+        const wrapper = mount(<App/>);
+
+        expect(wrapper.find('.hello').text()).toContain("Hello");
+    })
+})
